59130c453e322585dfb0ca866dfa7af89433f134
mysteriousdog <3076777685@qq.com>
2021-05-28 23:19:14 +0800

    client --xiufu

--- HandleMsg.cpp
+++ HandleMsg.cpp
@@ -25,7 +25,6 @@ using namespace std;
 
 msgHandle g_msgHandle[] = {
     {MSG_CMD, handleMsgCmd},
-    {MSG_CONNECT, handleUserConnectMsg},
     {MSG_TALK, handleUserSendMsg},
     {MSG_REG, handleUserRegMsg},
     {MSG_REG_REFUSE, handleUserRegRefusedMsg},
